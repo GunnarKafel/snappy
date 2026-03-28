bf15353ce892eb24dd20248834ba5d9571a0cb79
Jake Korvemaa <55055481+korvemaa@users.noreply.github.com>
2026-03-28 23:24:18 +1100

    Disable the origin select popover (whoops)

--- view3d_ui.py
+++ view3d_ui.py
@@ -7,14 +7,13 @@ def draw_VIEW3D_HT_header(self, context):
     layout = layout.box()
 
     row = layout.row(align=True)
-    row.popover(panel=pivot.OriginSelectPanel.bl_idname, text="", icon="OBJECT_ORIGIN")
+    # row.popover(panel=pivot.OriginSelectPanel.bl_idname, text="", icon="OBJECT_ORIGIN")
 
     formatted_scale = f"{context.space_data.overlay.grid_scale:.3f}"
     formatted_scale = formatted_scale.rstrip('0').rstrip('.')
     grid_text = f"{formatted_scale}{utility.unit()}"
 
-    
-    row.ui_units_x = 6.5
+    row.ui_units_x = 5
     row.menu(menu=snapping.GridPresetsMenu.bl_idname, text=grid_text, translate=False, icon='NONE')
 
     decrement = row.operator(operator=snapping.ScaleGrid.bl_idname, text="", icon='REMOVE')
